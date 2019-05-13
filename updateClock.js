3ce24dc8bdb17698625788b4a8251ea516d27839
victor vinci <victorvinci@protonmail.com>
2019-05-13 19:41:50 +0200

    add js file with first steps of the seconds function

--- updateClock.js
+++ updateClock.js
@@ -1,7 +1,7 @@
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsInDegrees = (seconds / 60) * 360 
+  const secondsInDegrees = (seconds / 60) * 360;
 }
 
 setInterval(setDate, 1000);

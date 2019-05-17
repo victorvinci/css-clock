37e9230a099f0b6f4b39aa86c0ed204ee52d5930
victor vinci <victorvinci@protonmail.com>
2019-05-17 20:03:25 +0200

    add function to get the correct time

--- updateClock.js
+++ updateClock.js
@@ -1,7 +1,24 @@
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
 function setDate() {
   const now = new Date();
+
+  // seconds
   const seconds = now.getSeconds();
-  const secondsInDegrees = (seconds / 60) * 360;
+  const secondsInDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
+
+  //minutes
+  const minutes = now.getMinutes();
+  const minutesInDegrees = ((minutes / 60) * 360 + 90);
+  minHand.style.transform = `rotate(${minutesInDegrees}deg)`;
+
+  //hours
+  const hours = now.getHours();
+  const hoursInDegrees = ((hours / 60) * 360 - 90);
+  hourHand.style.transform = `rotate(${hoursInDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);

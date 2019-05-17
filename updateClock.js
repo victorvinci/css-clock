5504cda17aec1fc04ce173732a529769d4b6bece
victor vinci <victorvinci@protonmail.com>
2019-05-17 22:18:12 +0200

    basic digital clock

--- updateClock.js
+++ updateClock.js
@@ -1,27 +1,32 @@
-const hourHand = document.querySelector('.hour-hand');
-const minHand = document.querySelector('.min-hand');
-const secondHand = document.querySelector('.second-hand');
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.min');
+const second = document.querySelector('.second');
 
 function setDate() {
   const now = new Date();
 
   // seconds
   const seconds = now.getSeconds();
-  const secondsInDegrees = seconds * 6;
-  //const secondsInDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsInDegrees + 90}deg)`;
+  if (seconds <= 9) {
+    second.innerHTML = "0" + seconds;
+  }else{
+    second.innerHTML = seconds;
+  }
 
   //minutes
   const minutes = now.getMinutes();
-  const minutesInDegrees = minutes * 6;
-  // const minutesInDegrees = ((minutes / 60) * 360) + 90;
-  minHand.style.transform = `rotate(${minutesInDegrees + 90}deg)`;
-
+  if (minutes <= 9) {
+    min.innerHTML = "0" + minutes + ":";
+  } else {
+    min.innerHTML = minutes + ":";
+  }
   //hours
   const hours = now.getHours();
-  const hoursInDegrees = hours * 30;
-  // const hoursInDegrees = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursInDegrees + 90}deg)`;
+  if (hours <= 9) {
+    hour.innerHTML = "0" + hours + ":";
+  } else {
+    hour.innerHTML = hours + ":";
+  }
 }
 
 setInterval(setDate, 1000);

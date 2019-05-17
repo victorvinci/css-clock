2fe2c4318d5bc00eab2e82916a95e60e7670d133
victor vinci <victorvinci@protonmail.com>
2019-05-17 21:37:41 +0200

    finish without transition due bug could not solve

--- updateClock.js
+++ updateClock.js
@@ -7,18 +7,22 @@ function setDate() {
 
   // seconds
   const seconds = now.getSeconds();
-  const secondsInDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
+  const secondsInDegrees = seconds * 6;
+  //const secondsInDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsInDegrees + 90}deg)`;
 
   //minutes
   const minutes = now.getMinutes();
-  const minutesInDegrees = ((minutes / 60) * 360) + 90;
-  minHand.style.transform = `rotate(${minutesInDegrees}deg)`;
+  const minutesInDegrees = minutes * 6;
+  // const minutesInDegrees = ((minutes / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minutesInDegrees + 90}deg)`;
 
   //hours
   const hours = now.getHours();
-  const hoursInDegrees = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursInDegrees}deg)`;
+  const hoursInDegrees = hours * 30;
+  // const hoursInDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursInDegrees + 90}deg)`;
 }
 
 setInterval(setDate, 1000);
+// setDate();

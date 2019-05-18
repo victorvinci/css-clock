bc07139a16b1b2de8397704788aea647b26c8bd2
victor vinci <victorvinci@protonmail.com>
2019-05-18 19:47:14 +0200

    set a default timezone and calculate how long until sleep on each timezone

--- updateClock.js
+++ updateClock.js
@@ -1,34 +1,44 @@
 const BR = document.querySelector('.BR');
 const IT = document.querySelector('.IT');
+const dormeIT = document.querySelector('.dormeIT');
+const dormeBR = document.querySelector('.dormeBR');
+
+moment.tz.setDefault("America/Sao_Paulo"); //set this as default due '.to' not working with '.tz' method
+
 let sunTimeBR = Boolean;
 let sunTimeIT = Boolean;
 
 function getDateBR() {
-  const horarioBR = moment().tz("America/Sao_Paulo").format('HH:mm:ss');
-  const horaBR = moment().tz("America/Sao_Paulo").format('HH');
+  const horarioBR = moment().format('HH:mm:ss');
+  const horaBR = moment().format('HH');
+  const dormirBR = moment().to(moment("01:00:00", "HH:mm:ss").add(1, 'day'));
 
-  if (horaBR < 24 && horaBR > 06 && horaBR <= 18) {
+  if (horaBR < 24 && horaBR > 06 && horaBR <= 19) {
     sunTimeBR = true;
   } else {
     sunTimeBR = false;
   }
   
   BR.innerHTML = "BR SP " + horarioBR;
+  dormeBR.innerHTML = "Sleep " + dormirBR; 
 
   return sunTimeBR;
 }
 
 function getDateIT() {
-  const horarioIT = moment().tz("Europe/Rome").format('HH:mm:ss');
-  const horaIT = moment().tz("Europe/Rome").format('HH');
+  const horarioIT = moment().add(5, 'hour').format('HH:mm:ss'); // add +5 for Rome timezone
+  const horaIT = moment().add(5, 'hour').format('HH');
+  const dormirIT = moment().add(5, 'hour').to(moment("03:00:00", "HH:mm:ss").add(1, 'day')); // calculate the amount of hours until the next time to sleep
+  const acordarIT = 11;
 
-  if (horaIT < 24 && horaIT > 06 && horaIT <= 18) {
+  if (horaIT < 24 && horaIT > 06 && horaIT <= 19) {
     sunTimeIT = true;
   } else {
     sunTimeIT = false;
   }
 
   IT.innerHTML = "IT ROME " + horarioIT;
+  dormeIT.innerHTML = "Sleep " + dormirIT; 
 
   return sunTimeIT;
 }
@@ -36,11 +46,8 @@ function getDateIT() {
 setInterval(getDateBR, 1000);
 setInterval(getDateIT, 1000);
 
-// console.log(getDateBR())
-// console.log(getDateIT())
-
 if (getDateBR() === true && getDateIT() === true){
-  console.log("mozi")
+  console.log("acorda");
 } else {
   console.log("dorme");
 }

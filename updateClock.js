f5169030f0e5dc2e69ece6896cf812a1fbbea9a0
victor vinci <victorvinci@protonmail.com>
2019-05-20 01:01:41 +0200

    try to fix a wierd bug when clock reachs 18:30 (br) and 00:30(it)

--- updateClock.js
+++ updateClock.js
@@ -15,9 +15,7 @@ let talkIT = Boolean;
 function getDateBR() {
   const horarioBR = moment().subtract(1, 'hour').format('HH:mm:ss');
   const horaBR = moment().subtract(1, 'hour').format('HH');
-  const dormirBR = moment().subtract(1, 'hour').to(moment("01:00:00", "HH:mm:ss")); //1h am
-  //const dormindoBR = 01;
-  const acordaBR = moment("01:00:00", "HH:mm:ss").add(8, 'hour').format('HH');;
+  const dormirBR = moment().subtract(1, 'hour').to(moment("01:00:00", "HH:mm:ss").add(1, 'day')); //1h am
 
   BR.innerHTML = "BR CG " + horarioBR;
 
@@ -27,9 +25,12 @@ function getDateBR() {
     dayTimeBR.innerHTML = "Sun is gone";
   }
   
-  if (horaBR >= 01 && horaBR < acordaBR) {
+  if (horaBR >= 01 && horaBR < 09) {
     dormeBR.innerHTML = "Sleeping";
     talkBR = false;
+  } else if (horaBR >= 18) { //there is a bug when the moment's clock reachs 19:00. This seems to solve it
+    dormeBR.innerHTML = "Sleep " + moment().subtract(1, 'hour').to(moment("01:00:00", "HH:mm:ss"));
+    talkBR = true;
   } else {
     dormeBR.innerHTML = "Sleep " + dormirBR;
     talkBR = true;
@@ -37,20 +38,12 @@ function getDateBR() {
 
   return talkBR;
 
-    // if (horaBR == meiaNoite || horaBR >= 11) {
-    
-    // } else {
-    
-    // }
-
 }
 
 function getDateIT() {
   const horarioIT = moment().add(5, 'hour').format('HH:mm:ss'); // add +5 for Rome timezone
   const horaIT = moment().add(5, 'hour').format('HH');
-  const dormirIT = moment().add(5, 'hour').to(moment("03:00:00", "HH:mm:ss")); // calculate the amount of hours until the next time to sleep
-  //const dormindoIT = moment("03:00:00", "HH:mm:ss").format('HH'); //3h am
-  //const acordaIT = moment("03:00:00", "HH:mm:ss").add(8, 'hour').format('HH'); //11h am
+  const dormirIT = moment().add(5, 'hour').to(moment("03:00:00", "HH:mm:ss").add(1, 'day')); // calculate the amount of hours until the next time to sleep
 
   IT.innerHTML = "IT ROME " + horarioIT;
 
@@ -63,6 +56,9 @@ function getDateIT() {
   if (horaIT >= 03 && horaIT < 11) {
     dormeIT.innerHTML = "Sleeping";
     talkIT = false;
+  } else if (horaIT <= 02) {
+    dormeIT.innerHTML = "Sleep " + moment().add(5, 'hour').to(moment("03:00:00", "HH:mm:ss")); //prevents odd bug when clock reachs 00:30 IT time
+    talkIT = true; 
   } else {
     dormeIT.innerHTML = "Sleep " + dormirIT;
     talkIT = true; 
@@ -79,7 +75,3 @@ if (getDateBR() === true && getDateIT() === true){
 } else {
   talkTime.innerHTML = "not a good time";
 }
-
-console.log((moment("01:00:00", "HH:mm:ss").from(moment().subtract(1, 'hour'))), (moment("03:00:00", "HH:mm:ss").from(moment().add(5, 'hour')))); // maybe a solution for the ss bug or using fromNow
-
-// .add(1, 'day') removed from sleep timer

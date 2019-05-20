c9c81b46bd47ec67114b9ba3b9bc5a7689ae6d43
victor vinci <victorvinci@protonmail.com>
2019-05-21 00:18:14 +0200

    change onclick to addEventListener

--- transitionsOnClick.js
+++ transitionsOnClick.js
@@ -3,23 +3,20 @@ const containerBR = document.querySelector('.container-brasil');
 const containerIT = document.querySelector('.container-italia');
 const infosBR = document.querySelectorAll('.info-br');
 const infosIT = document.querySelectorAll('.info-it');
-// talk.style.position = 'absolute';
-// containerBR.style.position = 'absolute';
-// containerIT.style.position = 'absolute';
 
-talk.onclick = function expandTalk() {
-  if (!talk.classList.contains('talk-shrink')) {
-    talk.style.transition = 'ease-in 0.5s';
-    talk.classList.add('talk-shrink');
+talk.addEventListener('click', function expandTalk() {
+  if (!this.classList.contains('talk-shrink')) {
+    this.style.transition = 'ease-in 0.5s';
+    this.classList.add('talk-shrink');
   } else {
-    talk.classList.remove('talk-shrink');
+    this.classList.remove('talk-shrink');
   }
-};
+});
 
-containerBR.onclick = function expandBR() {
-  if (containerBR.classList.contains('shrink')) {
-    containerBR.classList.remove('shrink');
-    containerBR.classList.add('expand');
+containerBR.addEventListener('click', function expandBR() {
+  if (this.classList.contains('shrink')) {
+    this.classList.remove('shrink');
+    this.classList.add('expand');
     setTimeout(function() {
       containerIT.classList.add('hide');
       infosBR[0].classList.add('expand-infos');
@@ -27,8 +24,8 @@ containerBR.onclick = function expandBR() {
       talk.classList.add('talk-zero-height');
     }, 500);
   } else {
-    containerBR.classList.remove('expand');
-    containerBR.classList.add('shrink');
+    this.classList.remove('expand');
+    this.classList.add('shrink');
     containerIT.classList.remove('hide');
     infosBR[0].classList.remove('expand-infos');
     infosBR[1].classList.remove('expand-infos');
@@ -38,12 +35,12 @@ containerBR.onclick = function expandBR() {
       containerIT.style['z-index'] = '0';
     }, 480);
   }
-};
+});
 
-containerIT.onclick = function expandIT() {
-  if (containerIT.classList.contains('shrink')) {
-    containerIT.classList.remove('shrink');
-    containerIT.classList.add('expand');
+containerIT.addEventListener('click', function expandIT() {
+  if (this.classList.contains('shrink')) {
+    this.classList.remove('shrink');
+    this.classList.add('expand');
     setTimeout(function() {
       containerBR.classList.add('hide');
       infosIT[0].classList.add('expand-infos');
@@ -51,11 +48,11 @@ containerIT.onclick = function expandIT() {
       talk.classList.add('talk-zero-height');
     }, 500);
   } else {
-    containerIT.classList.add('shrink');
+    this.classList.add('shrink');
     containerBR.classList.remove('hide');
-    containerIT.classList.remove('expand');
+    this.classList.remove('expand');
     infosIT[0].classList.remove('expand-infos');
     infosIT[1].classList.remove('expand-infos');
     talk.classList.remove('talk-zero-height');
   }
-};
+});
